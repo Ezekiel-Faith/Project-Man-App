11445c5bc0f31f346b1548f535d5c5788ba4d92d
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-03-17 05:43:35 +0100

    added 'NewProject' component and a reusable 'Input' component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import ProjectsSidebar from './components/ProjectsSidebar';
+import NewProject from './components/NewProject';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className='h-screen my-8'>
+    <main className='h-screen my-8 flex gap-8'>
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
