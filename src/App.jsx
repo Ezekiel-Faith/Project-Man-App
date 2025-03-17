9ff6ad5496a70ae46d4b3333d534d614a81cdca0
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-03-17 05:06:57 +0100

    added the ProjectsSidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import ProjectsSidebar from './components/ProjectsSidebar';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className='min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl'>
-        Tailwind CSS v3 is working! 🎉
-      </div>
-    </>
+    <main>
+      <ProjectsSidebar />
+    </main>
   );
 }
 
