e65f19806c71d1086285d2d7d348ddf67f8bf1ac
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-03-17 06:39:10 +0100

    made 'Button' component reusable, added 'NoProjectselected' component and modified the button in the 'ProjectsSidebar'

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import ProjectsSidebar from './components/ProjectsSidebar';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
   return (
     <main className='h-screen my-8 flex gap-8'>
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
